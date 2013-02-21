8fed08f9465b2f351fb66307cce957cf860de094
drellowramadas <drellowramadas@appacademy.io>
2013-02-21 13:43:34 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impstore::Application.config.secret_token = 'ad5ec27e4483d022f617b74ad832735d6c64c38af5433cf97d15ee24f518968436d30ceb7f69da535553bcae281317b1a13a21b3e2a6dac07717615421a2082c'
